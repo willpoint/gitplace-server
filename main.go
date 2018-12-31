53a82fb51327f095aa4ad42f7714d9b583e884ea
Uzondu Enudeme <willpoint@gmail.com>
2018-12-31 17:43:01 +0100

    Add a .gitignore file
    - ignore gitPlace - the name of the binary after running 'go build'

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"bytes"
+	"archive/zip"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os/exec"
@@ -11,98 +12,175 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+const (
+	headerContentType     = "Content-Type"
+	headerApplicationJSON = "application/json"
+)
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin:     func(_ *http.Request) bool { return true },
 }
 
-// commandType is a constant definition of allowed command types
-type commandType string
+func zipper() {
+	_ = zip.Deflate
+}
 
-const (
-	version commandType = "version"
-	history             = "history"
-	diff                = "diff"
-	branch              = "branch"
-)
-
-type gitter struct{ name string }
+// The Gitter struct
+type Gitter struct {
+	Name   string
+	Logger *log.Logger
+}
 
 // The Result type is composed of the type and
 // data produced by processMessage()
 type Result struct {
-	Typ  string
-	Data string
+	Type string `json:"type"`
+	Data string `json:"data"`
 }
 
-// JSON marshals returns the JSON value of result
-func (r Result) JSON() ([]byte, error) {
-	return json.Marshal(r)
+// ReqBody from client
+type ReqBody struct {
+	Type string `json:"type"`
+	Body string `json:"body"`
+}
+
+// NewGitter sets up Gitter with the default
+func NewGitter(name string, logger *log.Logger) *Gitter {
+	logger.SetFlags(log.LUTC)
+	logger.SetPrefix(name + ": ")
+	return &Gitter{name, logger}
 }
 
 // processMessage processes the message and returns a
 // any error it encounters during the message processing
-func (g *gitter) processMessage(msgs ...string) (result []byte, err error) {
-	//the first item in msgs is the type of message
+func (g *Gitter) processMessage(msgs ...string) (result interface{}, err error) {
+
 	var commands []string
 	if len(msgs) >= 1 {
 		commands = msgs[1:]
 	}
 	cmd := exec.Command("git", commands...)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err = cmd.Run()
+	output, err := cmd.Output()
 	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			return nil, exitErr
+		}
 		return nil, err
 	}
-	res := Result{msgs[0], out.String()}
-	return res.JSON()
+	res := Result{msgs[0], string(output)}
+	return res, nil
 }
 
-func (g *gitter) writeMessage(conn *websocket.Conn, msg []byte) error {
+func (g *Gitter) writeMessage(conn *websocket.Conn, msg interface{}) error {
 	return conn.WriteJSON(msg)
 }
 
-func (g *gitter) handleError(conn *websocket.Conn, msg string) {
-	conn.WriteMessage(websocket.TextMessage, []byte(msg))
+func (g *Gitter) handleError(conn *websocket.Conn, msg string) {
+	res := Result{"error", msg}
+	conn.WriteJSON(res)
 }
 
-func (g *gitter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (g *Gitter) handleHTTPError(w http.ResponseWriter, msg string) {
+	res := Result{"error", msg}
+	json, err := json.Marshal(res)
+	if err != nil {
+		w.Header().Set(headerContentType, headerApplicationJSON)
+		w.Write([]byte(`"type": "error", "body": "an unexpected error occured!"`))
+		return
+	}
+	w.Write(json)
+}
+
+func (g *Gitter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	if !websocket.IsWebSocketUpgrade(r) {
+		rb := ReqBody{}
+		err := json.NewDecoder(r.Body).Decode(&rb)
+		if err != nil {
+			g.handleHTTPError(w, err.Error())
+			return
+		}
+		command := rb.Type + " " + rb.Body
+		res, err := g.processMessage(strings.Fields(command)...)
+
+		if err != nil {
+			fmt.Println(err)
+			g.handleHTTPError(w, err.Error())
+			return
+		}
+		rr, err := json.Marshal(res)
+		if err != nil {
+			g.handleHTTPError(w, err.Error())
+			return
+		}
+		w.Header().Set(headerContentType, headerApplicationJSON)
+		w.Write(rr)
+		return
+	}
+
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		handleErr(err)
+		g.handleError(conn, err.Error())
+		return
 	}
 
 	for {
 		_, p, err := conn.ReadMessage()
 		if err != nil {
 			g.handleError(conn, "error reading the message: "+err.Error())
+			return
 		}
 		msgs := strings.Fields(string(p))
 		result, err := g.processMessage(msgs...)
 		if err != nil {
 			g.handleError(conn, err.Error())
+			return
 		}
 		err = g.writeMessage(conn, result)
 		if err != nil {
 			g.handleError(conn, err.Error())
+			return
 		}
 	}
 
 }
 
-// This example demonstrates a trivial echo server.
 func main() {
-	http.Handle("/echo", &gitter{"gitPlace"})
-	err := http.ListenAndServe(":12345", nil)
-	handleErr(err)
+
+	// TODO(uz)
+	// use flags to set/get required data to run
+	// eg. URL, enableLog(bool)...
+
+	// Logger
+	logger := &log.Logger{}
+
+	// ServeMux
+	mux := http.NewServeMux()
+
+	// httpUpgrade Handler
+	mux.Handle("/echo", NewGitter("httpUpgrade", logger))
+
+	// http-CORSEnabled Handler
+	mux.Handle("/command", AllowCors(CorsConfig{
+		AllowHeaders: []string{headerContentType},
+	})(NewGitter("http", logger)))
+
+	server := http.Server{
+		Addr:    ":12345",
+		Handler: mux,
+	}
+
+	// start server
+	handleErr(server.ListenAndServe())
+
 }
 
 func handleErr(err error, msg ...string) {
 	if err != nil {
 		log.SetPrefix("gitPlace: ")
 		log.SetFlags(log.Lshortfile | log.LUTC)
-		log.Println(err, msg)
+		log.Println("an error occured: ", err, msg)
 	}
 }
